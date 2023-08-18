0d0108b29e60688bb0351b2e2c698a148fe571f0
xuanduc1506 <nxduc150600@gmail.com>
2023-08-18 19:55:12 +0700

    Add AWS IAM

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     html = f"<h2>Hello my name is Nguyễn Xuân Đức. This is my capstone project about Uda_devops</h2>"
-    return html.formsssat(format)
+    return html.formsssat(format)dsdsdsdsd
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
